ae0d499bd71b2eb7084023942832fc340c4243f7
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-10-09 15:45:42 +0900

    add sp nav

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,37 @@
+"use client";
 import Link from 'next/link'
 import 'font-awesome/css/font-awesome.min.css';
+import { useEffect, useRef } from "react"
 
 export const Header = () => {
+	const navLinks = useRef<HTMLElement[]>([]);
+	const spNav = useRef<HTMLElement | null>(null);
+  useEffect(() => {
+    navLinks.current = Array.from(document.querySelectorAll(".pc-nav a")); // NodeListをHTMLElementの配列に変換
+    spNav.current = document.querySelector(".sp-nav");
+  
+    if (navLinks.current.length > 0 && spNav.current) {
+      navLinks.current.forEach(link => {
+        spNav.current!.appendChild(link.cloneNode(true));
+      });
+    }
+
+    let navToggle = document.getElementById('nav-toggle');
+    let body = document.body;
+
+    if (navToggle) {
+      navToggle.addEventListener('click', function() {
+        body.classList.toggle('open');
+      });
+    }
+  }, []); 
+
   return (
+    <>
     <header className={'header'}>
         <div className={'header-inner'}>
             <a href="/" className={'logo'}>NEXT TECH</a>
-            <nav>
+            <nav className={'pc-nav'}>
                 <Link href="/about">ABOUT</Link>
                 <Link href="/service">SERVICE</Link>
                 <Link href="/blog">BLOG</Link>
@@ -19,5 +44,23 @@ export const Header = () => {
           <li><Link href="https://sohtani.pythonanywhere.com/" target="_blank"><i className="fa fa-pencil"></i></Link></li>
         </ul>
     </header>
+    <div id="nav-toggle">
+      <div>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div id="sp-nav">
+      <div className={"sp-nav-inner"}>
+        <div className={"sp-nav-links"}>
+            <p className={"logo"}>
+              <Link href="/">Next Tech</Link>
+            </p>
+            <nav className="sp-nav"></nav>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
