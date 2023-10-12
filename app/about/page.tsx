9d7499fc549244811300406cba0c5417f64e6491
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-10-12 15:51:30 +0900

    update css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'About'
@@ -6,20 +7,49 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <section id="about">
+    <section id="blog">
     <div className={'under-inner'}>
       <h2>ABOUT</h2>
     </div>
     <div className={'container-inner'}>
-      <h3>VISION</h3>
-      <p>テクノロジーが日々進化する中で、常に最前線で最高のサービスを提供することを目指しています。私のビジョンは、クライアントが目指す成功をテクノロジーで実現するパートナーであることです。</p>
-      
-      <h3>MISSION</h3>
-      <p>私のミッションは、クライアントのビジネスを理解し、それを高度なテクノロジーで具現化することです。単なる開発者ではなく、ビジネスの成功を共に目指すパートナーでありたいと考えています。</p>
-      
-      <h3>REASON</h3>
-      <p>テクノロジーの進化がビジネスに多大な影響を与える現代において、「テクノロジーでビジネスをより良く、より効率的にする」という信念のもと、この事業を始めました。クライアントが抱える問題を解決し、ビジネスを次のレベルに引き上げることで、社会に貢献できると信じています。</p>
-      
+      <div className={'pageNav'}>
+        <ul className={'pageNav__list'}>
+          <li>
+            <Link href="#a01">
+              VISION
+            </Link>
+          </li>
+          <li>
+            <Link href="#a02">
+              MISSION
+            </Link>
+          </li>
+          <li>
+            <Link href="#a03">
+              REASON
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <section id="a01" className={'about'}>
+        <div className={'about-inner'}>
+          <h3>VISION</h3>
+          <p>テクノロジーが日々進化する中で、常に最前線で最高のサービスを提供することを目指しています。私のビジョンは、クライアントが目指す成功をテクノロジーで実現するパートナーであることです。</p>
+        </div>
+      </section>
+      <section id="a02" className={'about'}>
+        <div className={'about-inner'}>
+        <h3>MISSION</h3>
+        <p>私のミッションは、クライアントのビジネスを理解し、それを高度なテクノロジーで具現化することです。単なる開発者ではなく、ビジネスの成功を共に目指すパートナーでありたいと考えています。</p>
+        </div>
+      </section>
+      <section id="a03" className={'about'}>
+        <div className={'about-inner'}>
+          <h3>REASON</h3>
+          <p>テクノロジーの進化がビジネスに多大な影響を与える現代において、「テクノロジーでビジネスをより良く、より効率的にする」という信念のもと、この事業を始めました。クライアントが抱える問題を解決し、ビジネスを次のレベルに引き上げることで、社会に貢献できると信じています。</p>
+        </div>
+      </section>
     </div>
     </section>
   )
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,28 +11,24 @@ export default function Page() {
         <h2>CONTACT</h2>
       </div>
       <div className={'container-inner'}>
-      <form action="/submit" method="post">
-        <div className={'form-group'}>
-          <label htmlFor="name">名前（必須）</label>
-          <input type="text" id="name" name="name" required />
-        </div>
-        
-        <div className={'form-group'}>
-          <label htmlFor="email">メールアドレス（必須）</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-
-        <div className={'form-group'}>
-          <label htmlFor="subject">件名</label>
-          <input type="text" id="subject" name="subject" />
-        </div>
-
-        <div className={'form-group'}>
-          <label htmlFor="message">メッセージ</label>
-          <textarea id="message" name="message"></textarea>
-        </div>
-
-        <button type="submit">送信</button>
+      <p className={'under-read'}>下記項目をご記入の上送信ください。</p>
+      
+      <form action="/submit" method="post" className={'form'}>
+        <dl>
+          <dt>Name</dt>
+          <dd>
+            <input type="text" className={'textarea'} name="name" />
+          </dd>
+          <dt>Email</dt>
+          <dd>
+            <input type="text" className={'textarea'} name="emai" />
+          </dd>
+          <dt>Message</dt>
+          <dd>
+            <textarea name="message" className={'textarea'} />
+          </dd>
+        </dl>
+        <button type="submit">Submit</button>
       </form>
       </div>
     </section>
