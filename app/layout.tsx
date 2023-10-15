202d6e3828aa0e6cbc0abe99ff2145e7a676b3c2
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-10-15 13:17:47 +0900

    update fonts setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,20 @@
+import { Tulpen_One, Lato } from "next/font/google"
 import './styles/style.scss'
 import type { Metadata } from 'next'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+const tulpen_one = Tulpen_One({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-tulpen_One',
+  display: "swap",
+})
+const lato = Lato({
+  weight: '300',
+  subsets: ['latin'],
+  variable: '--font-lato',
+  display: "swap",
+})
 
 export const metadata: Metadata = {
   title: 'NEXT TECH',
@@ -14,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${tulpen_one.variable} ${lato.variable}`}>
       <body>
         <Header />
         <main className={'main'}>
