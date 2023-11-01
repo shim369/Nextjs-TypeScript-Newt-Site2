bbbb89b7d4ad9fcb6990394530a20d08dd2161d0
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-11-01 17:13:36 +0900

    update swiper

--- app/components/SwiperBox.tsx
+++ app/components/SwiperBox.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Image from 'next/image';
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Scrollbar, Autoplay, EffectFade } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "swiper/css/autoplay";
+import "swiper/css/effect-fade";
+import 'swiper/css/scrollbar';
 
 const images = [
     `/1.webp`,
@@ -18,10 +18,11 @@ const SwiperBox = () => {
     return (
         <>
             <Swiper
+                scrollbar={{hide: true}}
                 autoplay
-                navigation
+                effect={'fade'}
                 pagination={{ type: "bullets" }}
-                modules={[Navigation, Pagination, Autoplay]}
+                modules={[Scrollbar, Autoplay, EffectFade]}
                 className={'swiper-box'}
                 loop={true}
             >
