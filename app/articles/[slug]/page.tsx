13704ac576641b79f94ab6f1755d8bdbfc58283a
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-11-07 10:43:25 +0900

    update metadata

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -2,27 +2,38 @@ import { getArticles, getArticleBySlug } from '@/lib/newt'
 import type { Article } from '@/types/article'
 import type { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'BLOG'
-}
-
 type Props = {
   params: {
     slug: string
   }
 }
 
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+  const article = await getArticleBySlug(slug);
+  
+  if (!article) {
+    return {
+      title: 'BLOG',
+    };
+  }
+
+  return {
+    title: article.title,
+  };
+}
+
 export default async function Article({ params }: Props) {
-  const { slug } = params
-  const article = await getArticleBySlug(slug)
-  if (!article) return
+  const { slug } = params;
+  const article = await getArticleBySlug(slug);
+  if (!article) return null;
 
   return (
-      <section>
-        <div className={'under-inner'}>
-          <h2>BLOG</h2>
-        </div>
-        <div className={'blog-detail'}>
+    <section>
+      <div className={'under-inner'}>
+        <h2>BLOG</h2>
+      </div>
+      <div className={'blog-detail'}>
         <hr />
         <div className={'blog-content'}>
           <h1>{article.title}</h1>
@@ -32,7 +43,7 @@ export default async function Article({ params }: Props) {
           <div dangerouslySetInnerHTML={{ __html: article.body }} />
         </div>
         <hr />
-        </div>
-      </section>
-  )
+      </div>
+    </section>
+  );
 }
