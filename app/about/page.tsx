3b68ae05859db6cbce4c291f0a2c25359ee22f73
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-11-25 18:09:57 +0900

    import image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import type { Metadata } from 'next'
+import webp1 from '/public/1.webp';
+import webp2 from '/public/2.webp';
+import webp3 from '/public/3.webp';
 
 export const metadata: Metadata = {
   title: 'ABOUT'
@@ -37,7 +40,7 @@ export default function Page() {
           <div className={'about-inner'}>
             <h3>VISION</h3>
             <div className="about-flex">
-              <Image src="/1.webp" alt="VISION" width={600} height={400} />
+              <Image src={webp1} alt="VISION" width={600} height={400} />
               <p>In the rapid evolution of technology, I always bear the mission of providing the latest innovative services. My vision is to be a true partner in helping clients achieve their desired success through the power of technology. I strive to stand at the forefront of technology and provide innovative solutions.</p>
             </div>
           </div>
@@ -47,7 +50,7 @@ export default function Page() {
             <h3>MISSION</h3>
             <div className="about-flex">
               <p>My mission is to deeply understand the clients&apos; businesses and leverage advanced technology to produce tangible results. I value walking alongside our clients as true partners in the pursuit of business success, rather than being just developers.</p>
-              <Image src="/2.webp" alt="MISSION" width={600} height={400} />
+              <Image src={webp2} alt="MISSION" width={600} height={400} />
             </div>
           </div>
         </section>
@@ -55,7 +58,7 @@ export default function Page() {
           <div className={'about-inner'}>
             <h3>REASON</h3>
             <div className="about-flex">
-              <Image src="/3.webp" alt="REASON" width={600} height={400} />
+              <Image src={webp3} alt="REASON" width={600} height={400} />
               <p>The impact of the rapid evolution of technology on modern businesses is immeasurable. I launched this business based on the belief that &quot;technology can make businesses more effective and improved.&quot; I believe in solving the challenges faced by clients and elevating businesses to the next level through technology, thereby providing value to society and achieving success.</p>
             </div>
           </div>
