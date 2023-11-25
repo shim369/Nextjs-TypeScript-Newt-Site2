8740e770a3cd323cfc843820a867675e22651d15
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-11-25 18:24:11 +0900

    test import scss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Tulpen_One, Lato } from "next/font/google"
-// import './styles/style.scss'
+import './styles/style.scss'
 import type { Metadata } from 'next'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
