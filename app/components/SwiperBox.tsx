e04df81b4137d13ceb24953fd48267d874a97e56
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-11-30 19:32:53 +0900

    test swiper

--- app/components/SwiperBox.tsx
+++ app/components/SwiperBox.tsx
@@ -2,7 +2,8 @@
 import Image from 'next/image';
 import { Autoplay, EffectFade } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+// import "swiper/css";
+import "swiper/swiper.min.css";
 import "swiper/css/autoplay";
 import "swiper/css/effect-fade";
 import styles from '../styles/css/page.module.css'
