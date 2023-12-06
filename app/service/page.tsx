40e7c8a79079d17af0b67dd2fa89d0416c34ba8f
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-06 14:59:46 +0900

    add map function

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -6,6 +6,29 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
+  const services = [
+    {
+      title: "Website Development",
+      technologies: "HTML, CSS/SASS, JavaScript, Astro",
+      price: "¥300,000 ~"
+    },
+    {
+      title: "Web Application Development",
+      technologies: "Next.js, Nuxt.js, Python, Django",
+      price: "¥500,000 ~"
+    },
+    {
+      title: "CMS Construction",
+      technologies: "WordPress, MicroCMS, Newt",
+      price: "¥200,000 ~"
+    },
+    {
+      title: "Design & UI/UX",
+      technologies: "Figma, Adobe (Photoshop, Illustrator)",
+      price: "¥150,000 ~"
+    },
+  ];
+
   return (
     <section id="service">
       <div className={styles.underInner}>
@@ -15,66 +38,27 @@ export default function Page() {
         <p className={styles.underRead}>The prices listed above are all reference prices and may vary depending on the project&apos;s scope and requirements.<br />For more details, please inquire.</p>
 
         <ul className={styles.services}>
-          <li>
-            <h3 className={styles.tblTitle}>1. Website Development</h3>
-            <table className={styles.tbl}>
-              <tbody>
-                <tr>
-                  <th>Technologies</th>
-                  <td>HTML, CSS/SASS, JavaScript, Astro</td>
-                </tr>
-                <tr>
-                  <th>Price</th>
-                  <td>¥300,000 ~</td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
-          <li>
-            <h3 className={styles.tblTitle}>2. Web Application Development</h3>
-            <table className={styles.tbl}>
-              <tbody>
-                <tr>
-                  <th>Technologies</th>
-                  <td>Next.js, Nuxt.js, Python, Django</td>
-                </tr>
-                <tr>
-                  <th>Price</th>
-                  <td>¥500,000 ~</td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
-          <li>
-            <h3 className={styles.tblTitle}>3. CMS Construction</h3>
-            <table className={styles.tbl}>
-              <tbody>
-                <tr>
-                  <th>Technologies</th>
-                  <td>WordPress, MicroCMS, Newt</td>
-                </tr>
-                <tr>
-                  <th>Price</th>
-                  <td>¥200,000 ~</td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
-          <li>
-            <h3 className={styles.tblTitle}>4. Design & UI/UX</h3>
-            <table className={styles.tbl}>
-              <tbody>
-                <tr>
-                  <th>Technologies</th>
-                  <td>Figma, Adobe (Photoshop, Illustrator)</td>
-                </tr>
-                <tr>
-                  <th>Price</th>
-                  <td>¥150,000 ~</td>
-                </tr>
-              </tbody>
-            </table>
-          </li>
+          {services.map((service, index) => {
+            if (!service.title) return null;
+
+            return (
+              <li key={index}>
+                <h3 className={styles.tblTitle}>{index + 1}. {service.title}</h3>
+                <table className={styles.tbl}>
+                  <tbody>
+                    <tr>
+                      <th>Technologies</th>
+                      <td>{service.technologies}</td>
+                    </tr>
+                    <tr>
+                      <th>Price</th>
+                      <td>{service.price}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </li>
+            );
+          })}
         </ul>
 
         <h3 className={`${styles.tblTitle} ${styles.opTitle}`}>Additional Options</h3>
