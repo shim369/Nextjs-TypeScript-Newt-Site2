43c2aa71c7a4282321d41463108103ba86f549ab
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2023-12-10 14:24:39 +0900

    update gsap & css

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,12 @@ export default function Home() {
   const topLeftInnerRef = useRef<HTMLDivElement>(null);
   const topRightRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const videoDivRef = useRef<HTMLDivElement>(null);
+  const videoTextRef = useRef<HTMLHeadingElement>(null);
   const videoContainerRef = useRef<HTMLDivElement>(null);
   const contentsBoxRef = useRef<HTMLDivElement>(null);
   const slideVisualRef = useRef<HTMLDivElement>(null);
-  const leftDivRef = useRef<HTMLDivElement>(null);
-  const rightDivRef = useRef<HTMLDivElement>(null);
+  const slideTextRef = useRef<HTMLHeadingElement>(null);
   const contactRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -29,8 +30,8 @@ export default function Home() {
 
     const TL = Gsap.timeline();
 
-    TL.to(topLeftInner, { y: '0%', autoAlpha: 1, duration: 0.5, delay: 0.5, ease: 'power4.out' });
-    TL.to(topRight, { autoAlpha: 1, duration: 1, delay: 0.5, ease: 'power4.out' }, '-=0.5');
+    TL.to(topLeftInner, { y: '0%', autoAlpha: 1, duration: 0.5, delay: 0.3, ease: 'power4.out' });
+    TL.to(topRight, { autoAlpha: 1, duration: 1, delay: 0.3, ease: 'power4.out' }, '-=0.3');
   })
 
   useEffect(() => {
@@ -43,7 +44,7 @@ export default function Home() {
       end: 'center center',
       onEnter: () => {
         if (contentsBox) {
-          Gsap.to(contentsBox, { autoAlpha: 1, duration: 0.5, ease: 'power4.out' });
+          Gsap.to(contentsBox, { y: '0px', autoAlpha: 1, duration: 0.5 });
         }
       },
     });
@@ -51,34 +52,36 @@ export default function Home() {
 
   useEffect(() => {
     const slideVisual = slideVisualRef.current;
-    Gsap.set(slideVisual, { autoAlpha: 0 });
+    const slideText = slideTextRef.current;
+    Gsap.set(slideVisual, { y: '30px', autoAlpha: 0 });
     ScrollTrigger.create({
       trigger: slideVisual,
       start: 'top center',
       end: 'center center',
       onEnter: () => {
-        Gsap.to(slideVisual, { autoAlpha: 1, duration: 0.5, ease: 'power4.out' });
+        Gsap.to(slideVisual, { y: '0px', autoAlpha: 1, duration: 0.5 });
+        Gsap.to(slideText, { autoAlpha: 1, delay: 0.5, duration: 0.5, ease: 'power4.out' });
       },
     });
   });
 
   useEffect(() => {
     const videoContainer = videoContainerRef.current;
-    const leftDiv = leftDivRef.current;
-    const rightDiv = rightDivRef.current;
-  
+    const videoDiv = videoDivRef.current;
+    const videoText = videoTextRef.current;
+
     Gsap.to(videoRef.current, {
       scrollTrigger: {
         trigger: videoContainer,
         start: 'top center',
         end: 'bottom center',
         onEnter: () => {
-          Gsap.to(leftDiv, { width: 0, duration: 0.5 });
-          Gsap.to(rightDiv, { width: 0, duration: 0.5 });
+          Gsap.to(videoDiv, { autoAlpha: 0, duration: 0.3 });
+          Gsap.to(videoText, { autoAlpha: 1, delay: 0.5, duration: 0.5, ease: 'power4.out' });
         },
       },
     });
-  }, [videoRef]);  
+  }, [videoRef]);
 
   useEffect(() => {
     const contact = contactRef.current;
@@ -89,7 +92,7 @@ export default function Home() {
       start: 'top center',
       end: 'center center',
       onEnter: () => {
-        Gsap.to(contact, { y: '0%', autoAlpha: 1, duration: 0.5, ease: 'power4.out' });
+        Gsap.to(contact, { y: '0px', autoAlpha: 1, duration: 0.5 });
       },
     });
   });
@@ -108,11 +111,10 @@ export default function Home() {
         </div>
       </section>
       <section id="videoContainer" className={styles.videoContainer} ref={videoContainerRef}>
-        <h2 className={styles.videoText}>
+        <div className={styles.videoDiv} ref={videoDivRef}></div>
+        <h2 className={styles.videoText} ref={videoTextRef}>
           Increasing Business Velocity <br />through Technology
         </h2>
-        <div id="leftDiv" className={styles.leftDiv} ref={leftDivRef}></div>
-        <div id="rightDiv" className={styles.rightDiv} ref={rightDivRef}></div>
         <video
           className={styles.video}
           ref={videoRef}
@@ -133,7 +135,7 @@ export default function Home() {
       <section id="slideVisual" className={styles.slideVisual} ref={slideVisualRef}>
         <div id="slideVisualInner" className={styles.slideVisualInner}>
           <SlideBox />
-          <h3>Would you like to try <br className={styles.spBr} />the latest technology?</h3>
+          <h3 className={styles.slideText} ref={slideTextRef}>Would you like to try <br className={styles.spBr} />the latest technology?</h3>
         </div>
       </section>
       <section id="contact" className={styles.contact} ref={contactRef}>
